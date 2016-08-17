38958f289eb51a724700a93aa06953cff359b152
Jan Rocho <jan.rocho@fh-dortmund.de>
2016-08-17 13:28:49 +0200

    added configuration gui for muvin

---
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/Muvin/classes/class.ilMuvinConfigGUI.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ +-----------------------------------------------------------------------------+
+	| ILIAS open source                                                           |
+	+-----------------------------------------------------------------------------+
+	| Copyright (c) 1998-2009 ILIAS open source, University of Cologne            |
+	|                                                                             |
+	| This program is free software; you can redistribute it and/or               |
+	| modify it under the terms of the GNU General Public License                 |
+	| as published by the Free Software Foundation; either version 2              |
+	| of the License, or (at your option) any later version.                      |
+	|                                                                             |
+	| This program is distributed in the hope that it will be useful,             |
+	| but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+	| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+	| GNU General Public License for more details.                                |
+	|                                                                             |
+	| You should have received a copy of the GNU General Public License           |
+	| along with this program; if not, write to the Free Software                 |
+	| Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
+	+-----------------------------------------------------------------------------+
+ */
+
+include_once("./Services/Component/classes/class.ilPluginConfigGUI.php");
+ 
+/**
+ * MUVIN configuration user interface class
+ *
+ * @author Jan Rocho <jan.rocho@fh-dortmund.de>
+ * @version $Id$
+ *
+ */
+class ilMuvinConfigGUI extends ilPluginConfigGUI
+{
+	
+	/**
+	* Handles all commmands, default is "configure"
+	*/
+	function performCommand($cmd)
+	{
+
+		switch ($cmd)
+		{
+			case "configure":
+			case "save":
+				$this->$cmd();
+				break;
+		}
+	}
+
+	/**
+	 * Configure screen
+	 */
+	function configure()
+	{
+		global $tpl;
+
+		$pl = $this->getPluginObject();
+		ilUtil::sendInfo($pl->txt("nothing_to_configure"), false);
+		return;
+		
+
+		$form = $this->initConfigurationForm();
+		$tpl->setContent($form->getHTML());
+	}
+	
+	//
+	// From here on, this is just an example implementation using
+	// a standard form (without saving anything)
+	//
+	
+	/**
+	 * Init configuration form.
+	 *
+	 * @return object form object
+	 */
+	public function initConfigurationForm()
+	{
+		global $lng, $ilCtrl;
+		
+		$pl = $this->getPluginObject();
+	
+		include_once("Services/Form/classes/class.ilPropertyFormGUI.php");
+		$form = new ilPropertyFormGUI();
+	
+	
+		$form->addCommandButton("save", $lng->txt("save"));
+	                
+		$form->setTitle($pl->txt("plugin_configuration"));
+		$form->setFormAction($ilCtrl->getFormAction($this));
+		
+		return $form;
+	}
+	
+	/**
+	 * Save form input (currently does not save anything to db)
+	 *
+	 */
+	public function save()
+	{
+		global $tpl, $lng, $ilCtrl;
+	
+		$pl = $this->getPluginObject();
+		
+		$form = $this->initConfigurationForm();
+		if ($form->checkInput())
+		{
+			ilUtil::sendSuccess($pl->txt("saving_invoked"), true);
+			$ilCtrl->redirect($this, "configure");
+		}
+		else
+		{
+			$form->setValuesByPost();
+			$tpl->setContent($form->getHtml());
+		}
+	}
+
+}
+?>
