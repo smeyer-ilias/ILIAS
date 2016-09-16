82593acf7c754139e14e06114a1c1ae1bf02c3c7
Jan Rocho <jan.rocho@fh-dortmund.de>
2016-09-16 09:54:41 +0200

    added LiveVoting as a submodule

--- Customizing/global/plugins/Services/Repository/RepositoryObject/LiveVoting/plugin.php
+++
@@ -1,7 +0,0 @@
-<?php
-$id = 'xlvo';
-$version = '3.1.1';
-$ilias_min_version = '5.1.0';
-$ilias_max_version = '5.1.999';
-$responsible = 'studer + raimann ag';
-$responsible_mail = 'info at studer-raimann.ch';
